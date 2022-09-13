ffcbb383ff7b0cfccfa278680062b84c30f54243
kibbang <007kibbang@gmail.com>
2022-09-13 21:59:31 +0900

    vue에서의 데이터 출력 방법

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello Vue</h1>
+  <main-nav />
 </template>
 
+<!-- main-nav는 App.vue에서 실핻되는 하위요소 즉 부모:App.vue, 자식 main-nav 의 관계 -->
+
 <script>
+import MainNav from "@/components/MainNav";
 export default {
   name: "App",
+  components: {
+    MainNav: MainNav, // 컴포넌트 등록 (키 이름은 동일하게 해주는게 좋다.)
+  },
 };
 </script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="w-full text-sm">Main Nav</header>
+  <div class="fixed top-0 left-0 w-full h-16 bg-white">
+    <div
+      class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
+    >
+      <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+      <h2 class="flex items-center h-full ml-8">
+        Developed By {{ author.firstName }} {{ author.lastName }}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainNav.vue",
+  data() {
+    return {
+      company: "Shin Careers", // 데이터 바인딩 {{}} 안에 변수 이름 넣어주기
+      author: {
+        firstName: "kihyun",
+        lastName: "Shin",
+      }, // 변수가 정의 되지 않으면 공백으로 나옴 undefined를 처리하는 방식임
+    };
+  },
+};
+</script>
